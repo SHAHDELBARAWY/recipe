a7bd8f925c8ed07563a6cf35d5e9736458dff9c6
SHAHDELBARAWY <shahdelbarawy274@gmail.com>
2025-02-22 19:56:00 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import Layout from "./components/Layout.jsx";
+import Meals from "./components/Meals.jsx";
+import Area from "./components/Area.jsx";
+import Ingredients from "./components/Ingredients.jsx";
+import MealDetails from "./components/MealDetails.jsx";
+
+export default function App() {
+  const routes = createBrowserRouter([
+    {path:'/',element:<Layout></Layout>,children:[
+      {index:true,element:<Meals></Meals>},
+      {path:'/mealdetails',element:<MealDetails></MealDetails>},
+      {path:'/area',element:<Area></Area>},
+      {path:'/ingredients',element:<Ingredients></Ingredients>},
+    ]}
+  ])
+  return (
+    <>
+      <RouterProvider router={routes}/>
+    </>
+  )
+}
---
+++ src/components/LoadingDetails.jsx
@@ -0,0 +1,12 @@
+
+export default function LoadingDetails() {
+    return (
+        <div className="p-4 overflow-hidden sm:ml-64 min-h-screen">
+            <div className="container mt-5 relative">
+                <div className="bg-white absolute top-0 bottom-0 right-0 left-0 flex justify-center items-center">
+                   
+                </div>
+            </div>
+        </div>
+    )
+}
